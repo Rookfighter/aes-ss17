064cdaf978b4d16c9d2c6bbec5a7e9492a8beb9e
Fabian Meyer <user8324@posteo.net>
2017-05-21 19:07:45 +0200

    Make backlight and rs constant

--- ex02/lcd.vhd
+++ ex02/lcd.vhd
@@ -46,16 +46,17 @@ architecture behavioral of lcd is
     signal   cnt:    std_logic_vector(CNTLEN-1 downto 0) := (others => '0');
 
     -- instruction signal that splits into "rs + rw + data"
-    signal   ins:    std_logic_vector(5 downto 0);
+    signal   ins:    std_logic_vector(4 downto 0);
 begin
 
     -- carry bit of en_cnt defines enable signal
     en <= en_cnt(ENCNTLEN-1);
 
     -- map instruction signal to out ports
-    rs   <= ins(5);
-    rw   <= ins(4);
+    rs   <= ins(4);
     data <= ins(3 downto 0);
+    rw   <= '0';
+    bl   <= '1';
 
     process(rst, clk)
     begin
@@ -64,9 +65,7 @@ begin
             cnt <= (others => '0');
             en_cnt <= (others => '0');
             ins <= (others => '0');
-
             rdy <= '0';
-            bl <= '0';
         elsif rising_edge(clk) then
             cnt <= cnt + 1;
             -- always set enable to 0 again
@@ -81,11 +80,11 @@ begin
                     case to_integer(unsigned(cnt)) is
                         when 0 =>
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000000";
+                            ins <= "00000";
                         when 20 * CLKPERMS =>
                             -- after 20ms
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000011";
+                            ins <= "00011";
                         when 30 * CLKPERMS =>
                             -- after 10ms
                             en_cnt(ENCNTLEN-1) <= '1';
@@ -95,38 +94,38 @@ begin
                         when 30 * CLKPERMS + 200 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000010";
+                            ins <= "00010";
                         when 30 * CLKPERMS + 300 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
                         when 30 * CLKPERMS + 400 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "001100";
+                            ins <= "01100";
                         when 30 * CLKPERMS + 500 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000000";
+                            ins <= "00000";
                         when 30 * CLKPERMS + 600 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "001100";
+                            ins <= "01100";
                         when 30 * CLKPERMS + 700 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000000";
+                            ins <= "00000";
                         when 30 * CLKPERMS + 800 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000001";
+                            ins <= "00001";
                         when 40 * CLKPERMS + 800 * CLKPERUS =>
                             -- after 10ms
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "000000";
+                            ins <= "00000";
                         when 40 * CLKPERMS + 900 * CLKPERUS =>
                             -- after 100us
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "001100";
+                            ins <= "01100";
                         when 40 * CLKPERMS + 1000 * CLKPERUS - 1 =>
                             -- after 100us - 1 cycle, so we are in ready state
                             -- on next cycle to receive commands
@@ -143,20 +142,20 @@ begin
                         cnt <= (others => '0');
                         -- set already first instruction for writing data
                         en_cnt(ENCNTLEN-1) <= '1';
-                        ins <= "001" & posy & "00";
+                        ins <= "01" & posy & "00";
                     end if;
                 -- FLUSH state writes current char din at position (posx/posy)
                 when SFLUSH =>
                     case to_integer(unsigned(cnt)) is
                         when 100 * CLKPERUS =>
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "00" & posx;
+                            ins <= "0" & posx;
                         when 200 * CLKPERUS =>
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "10" & din(7 downto 4);
+                            ins <= "1" & din(7 downto 4);
                         when 300 * CLKPERUS =>
                             en_cnt(ENCNTLEN-1) <= '1';
-                            ins <= "10" & din(3 downto 0);
+                            ins <= "1" & din(3 downto 0);
                         when 400 * CLKPERUS - 1 =>
                             state <= SREADY;
                             rdy <= '1';
